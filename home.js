a8580080c6f897278221ac7cf96d58edd57ce829
Musammil <musammilaim786@gmail.com>
2026-02-05 04:06:36 +0530

    login sighn-up responsive

--- home.js
+++ home.js
@@ -1,8 +1,20 @@
 
-const signupButton = document.querySelector('.sign-up')
-signupButton.addEventListener("click", function (event) {
-    event.preventDefault()
-  location.assign("login.html");
+document.addEventListener("DOMContentLoaded", () => {
+
+  const signupButton = document.querySelector('.sign-up');
+  if (signupButton) {
+    signupButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      location.assign("login.html");
+    });
+  }
+
+  const menuBtn = document.querySelector(".fa-bars");
+  const mobNav = document.querySelector(".mob-nav");
+
+  menuBtn.addEventListener("click", () => {
+    mobNav.classList.toggle("active");
+    menuBtn.classList.add("hide")
+  });
 
 });
-
